daba6d6b8137b76ab9650e68696c7b26e26d2636
donghuazone <vaiya71525@gmail.com>
2026-01-13 15:45:06 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+const POSTS = [
+  {
+    id: "ep111",
+    title: "Apotheosis Episode 111",
+    thumb: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh2jwgYzc6zsOhWHl0lb1-f6khTSShrrzeZzmplxkuwUyp4tB-bolqu6LohClimHEgpMk2obe0P5Q3f7Qy4slL6LRa2GASmAKidqCVqqGA05Dh55yB7-xkg9wK7jrrxTfgrsRfU-MtBV8GJKkYY3EX8FNZ8alNnlzn0eeDc7hPwQCNLtfKGZCqK07sOqbU/s1920/008v2vQ8gy1i83uase0i0j30u01hckg5.jpg",
+    video: "https://geo.dailymotion.com/player/x110cm.html?video=k4cOuKILsE3T5DEEsua"
+  },
+  {
+    id: "ep112",
+    title: "Apotheosis Episode 112",
+    thumb: "https://i.imgur.com/ep112.jpg",
+    video: "https://www.dailymotion.com/embed/video/x8bbbb"
+  }
+];
